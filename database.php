62e06850238c3382e87004ef65742fb0eb9ce770
Trevor <trevor.h.crow@gmail.com>
2018-11-29 15:17:39 -0500

    added code to put the drinks in the drink_stock table

--- database.php
+++ database.php
@@ -23,6 +23,7 @@
     // sql to create tables
     $sql = "CREATE TABLE soda_stock(
     drink INT(2) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    drinkname varchar(21),
     stock INT(3)
     )";
     if ($conn->query($sql) === TRUE) {
@@ -46,5 +47,88 @@
         echo "Error creating table: " . $conn->error;
     }
 
+    $sql = "CREATE TABLE orders(
+        ordernum INT(12) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        userid int(6) UNSIGNED,
+        drink int(2) UNSIGNED,
+        bought_date TIMESTAMP
+    )";
+    if ($conn->query($sql) === TRUE) {
+        echo "Table orders created successfully";
+    } else {
+        echo "Error creating table: " . $conn->error;
+    }
+
+    //insert drinks into drinks table
+    $stmt = $conn->prepare("INSERT INTO soda_stock(drinkname, stock) VALUES(?, ?)");
+    $stmt->bind_param("si", "Rough Snake", 100);
+    if($conn->query($sql) === true){
+        $last_id = $conn->insert_id;
+    }else{
+        echo "Error creating Record: ".$conn->error;
+    }
+    $stmt->bind_param("si", "Lemon lager", 100);
+    if($conn->query($sql) === true){
+        $last_id = $conn->insert_id;
+    }else{
+        echo "Error creating Record: ".$conn->error;
+    }
+    $stmt->bind_param("si", "Passion fruit buzzer (kiwi java)", 100);
+    if($conn->query($sql) === true){
+        $last_id = $conn->insert_id;
+    }else{
+        echo "Error creating Record: ".$conn->error;
+    }
+    $stmt->bind_param("si", "SeÑor Bub", 100);
+    if($conn->query($sql) === true){
+        $last_id = $conn->insert_id;
+    }else{
+        echo "Error creating Record: ".$conn->error;
+    }
+    $stmt->bind_param("si", "Drop", 100);
+    if($conn->query($sql) === true){
+        $last_id = $conn->insert_id;
+    }else{
+        echo "Error creating Record: ".$conn->error;
+    }
+    $stmt->bind_param("si", "Brew POP", 100);
+    if($conn->query($sql) === true){
+        $last_id = $conn->insert_id;
+    }else{
+        echo "Error creating Record: ".$conn->error;
+    }
+    $stmt->bind_param("si", "Angelic lion", 100);
+    if($conn->query($sql) === true){
+        $last_id = $conn->insert_id;
+    }else{
+        echo "Error creating Record: ".$conn->error;
+    }
+    $stmt->bind_param("si", "EPIC(Fruit Punch)", 100);
+    if($conn->query($sql) === true){
+        $last_id = $conn->insert_id;
+    }else{
+        echo "Error creating Record: ".$conn->error;
+    }
+    $stmt->bind_param("si", "EPIC(Blue raspberry)", 100);
+    if($conn->query($sql) === true){
+        $last_id = $conn->insert_id;
+    }else{
+        echo "Error creating Record: ".$conn->error;
+    }
+    $stmt->bind_param("si", "Rough Snake Zero", 100);
+    if($conn->query($sql) === true){
+        $last_id = $conn->insert_id;
+    }else{
+        echo "Error creating Record: ".$conn->error;
+    }
+    $stmt->bind_param("si", "Oaken Five", 100);
+    if($conn->query($sql) === true){
+        $last_id = $conn->insert_id;
+    }else{
+        echo "Error creating Record: ".$conn->error;
+    }
+
+
+    $stmt->close();
     $conn->close();
 ?>
